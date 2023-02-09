5fa2b89b91ff5addf15cc8793eee47df2bf2db55
Yakubenka Ivan <palasja@gmail.com>
2023-02-09 15:38:26 +0300

    fix: remove cost from type in DB

--- Task12/DAL/Migrations/20230209123141_RemoveCostFromServicType.Designer.cs
+++ Task12/DAL/Migrations/20230209123141_RemoveCostFromServicType.Designer.cs
@@ -4,14 +4,16 @@ using DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(ContractContext))]
-    partial class ContractContextModelSnapshot : ModelSnapshot
+    [Migration("20230209123141_RemoveCostFromServicType")]
+    partial class RemoveCostFromServicType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
